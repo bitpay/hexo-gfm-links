af6d269218ebc2972790f0c0dff3ccc359d62f3e
Braydon Fuller <braydon@bitpay.com>
2015-09-29 15:49:52 -0400

    Fix replacing links in a directory.

--- test/fixtures/input.js
+++ test/fixtures/input.js
@@ -1,3 +1,3 @@
-var example = '<p>You can also instantiate an Address from a String, <a href="publickey.md">PublicKey</a>, or <a href="hierarchical.html">HDPublicKey</a>, in case you are not the owner of the private key.</p>';
+var example = '<p>You can also instantiate an Address from a String, <a href="publickey.md">PublicKey</a>, or <a href="hierarchical.html">HDPublicKey</a>, in case you are not the owner of the private key.</p><ul><li><ul><li><a href="services/address.md">Services</a></li></ul></li></ul>';
 
 module.exports = example;
